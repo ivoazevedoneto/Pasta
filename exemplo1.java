fa7aec7ea6bbd3573673b1e5032fc93cd042464a
ivoazevedoneto <67112692+ivoazevedoneto@users.noreply.github.com>
2020-06-18 13:23:05 -0300

    Exemplo 1
    Adicionando exemplo 1.

---
+++ exemplo1.java
@@ -0,0 +1,14 @@
+package aula1;
+//My first software, an example to be remembered!
+public class exemplo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Command to show messages!
+		System.out.println("SHAZAM KARAI!");
+		
+
+	}
+
+}
